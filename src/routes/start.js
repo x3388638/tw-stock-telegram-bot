4a3f1a4244eeaf57f40c6668c8335176b98ecea1
YY <z3388638@gmail.com>
2020-09-09 00:19:59 +0800

    Update start msg

--- src/routes/start.js
+++ src/routes/start.js
@@ -1,7 +1,20 @@
 const startHandler = (bot) => {
-  bot.onText(/\/start/, (msg) => {
+  bot.onText(/\/start$/, (msg) => {
     const chatId = msg.chat.id
-    bot.sendMessage(chatId, 'Welcome')
+    bot.sendMessage(
+      chatId,
+      `歡迎使用盤中飧守護神，守護你盤中的便當錢
+
+服務項目:
+\`/chart 2330\` - 查詢盤中即時走勢圖
+\`/text 2330\` - 查詢盤中即時價量及五檔
+
+資料來源: [Yahoo!奇摩股市](https://tw.stock.yahoo.com/)`,
+      {
+        parse_mode: 'Markdown',
+        disable_web_page_preview: true
+      }
+    )
   })
 }
 
