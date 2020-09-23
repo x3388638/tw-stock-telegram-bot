168b9bf239b4f19a50ae9ba9b6e86003a136f1c4
YY <z3388638@gmail.com>
2020-09-23 11:54:46 +0800

    Answer callback query

--- src/routes/callbackQuery.js
+++ src/routes/callbackQuery.js
@@ -1,9 +1,10 @@
 import { afterHoursCallbackQueryHandlers } from './afterHours'
 
 const handleCallbackQuery = (bot) => {
-  bot.on('callback_query', (query) => {
+  bot.on('callback_query', async (query) => {
     const handlers = { ...afterHoursCallbackQueryHandlers }
-    handlers[query.data](bot, query)
+    await handlers[query.data](bot, query)
+    bot.answerCallbackQuery(query.id)
   })
 }
 
