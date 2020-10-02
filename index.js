415fc587746555277aff64d145e99cac58944618
YY <z3388638@gmail.com>
2020-10-02 17:25:14 +0800

    Refactor route for news

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import { handleStart } from './src/routes/start'
 import { handleChart } from './src/routes/chart'
 import { handleText } from './src/routes/text'
 import { handleCandlestick } from './src/routes/candlestick'
+import { handleStockNews } from './src/routes/news'
 
 const routeConfig = {
   '^/start$': handleStart,
   '^/chart': handleChart,
   '^/text': handleText,
-  '^/[K|k]': handleCandlestick
+  '^/[K|k]': handleCandlestick,
+  '^/news(?: (.*))?$': handleStockNews
 }
 
 bot.route(routeConfig)
--- src/routes/news.js
+++ src/routes/news.js
@@ -1,39 +1,34 @@
 import { isStockIdValid, fetchStockNews } from '../lib/stock'
 import { getNewsListHTMLTemplate } from '../lib/template'
 
-const handleStockNews = (bot) => {
-  bot.onText(/\/news (.*)/, async (msg, match) => {
-    const chatId = msg.chat.id
-    const stockId = match[1]
+export async function handleStockNews(msg, match) {
+  const chatId = msg.chat.id
+  const stockId = match[1]
 
-    if (!isStockIdValid(stockId)) {
-      return bot.sendMessage(chatId, '請輸入有效股號\ne.g. `/news 2330`', {
-        parse_mode: 'Markdown'
-      })
-    }
-
-    const newsList = await fetchStockNews(stockId)
-    if (!newsList) {
-      return bot.sendStockIdNotFoundError(chatId, stockId)
-    }
-
-    if (!newsList.length) {
-      return bot.sendMessage(chatId, `查無 ${stockId} 相關新聞`)
-    }
-
-    const HTML = getNewsListHTMLTemplate(newsList)
-    bot.sendMessage(chatId, HTML, {
-      parse_mode: 'HTML',
-      disable_web_page_preview: true
-    })
-  })
-
-  bot.onText(/\/news$/, (msg) => {
-    const chatId = msg.chat.id
-    bot.sendMessage(chatId, '請帶入股號\ne.g. `/news 2330`', {
+  if (!stockId) {
+    return this.sendMessage(chatId, '請帶入股號\ne.g. `/news 2330`', {
       parse_mode: 'Markdown'
     })
+  }
+
+  if (!isStockIdValid(stockId)) {
+    return this.sendMessage(chatId, '請輸入有效股號\ne.g. `/news 2330`', {
+      parse_mode: 'Markdown'
+    })
+  }
+
+  const newsList = await fetchStockNews(stockId)
+  if (!newsList) {
+    return this.sendStockIdNotFoundError(chatId, stockId)
+  }
+
+  if (!newsList.length) {
+    return this.sendMessage(chatId, `查無 ${stockId} 相關新聞`)
+  }
+
+  const HTML = getNewsListHTMLTemplate(newsList)
+  this.sendMessage(chatId, HTML, {
+    parse_mode: 'HTML',
+    disable_web_page_preview: true
   })
 }
-
-export default handleStockNews
